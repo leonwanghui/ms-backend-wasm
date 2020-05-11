af00d76db3b1a6a35c4f70c84b25a4dbf1a522b2
leonwanghui <wanghui71leon@gmail.com>
2020-05-11 11:29:45 +0800

    Update the latest code

--- src/main.cc
+++ src/main.cc
@@ -13,32 +13,17 @@ inline T add(T a, T b) {
 namespace mindspore {
 namespace wasm {
 // We use C-style API to interface with Javascript.
-extern "C" {
+void Add() {
+  xnn_initialize(nullptr /* allocator */);
+  xnn_operator_t add_op = nullptr;
 
-// void Add(const size_t a_id, const size_t* a_shape_ptr, const size_t a_shape_len,
-//          const size_t b_id, const size_t* b_shape_ptr, const size_t b_shape_len,
-//          const DType dtype, const size_t out_id) {
-//   switch (dtype) {
-//     case DType::float32:
-//       binary_xnn_f32(a_id, a_shape_ptr, a_shape_len, b_id, b_shape_ptr,
-//                      b_shape_len, out_id, xnn_create_add_nd_f32,
-//                      xnn_setup_add_nd_f32);
-//       break;
-//     case DType::int32:
-//       binary_i32(a_id, b_id, out_id, add<int32_t>);
-//       break;
-//     case DType::boolean:
-//       binary_bool(a_id, b_id, out_id, add<bool>);
-//       break;
-//     default:
-//       break;
-//   }
-// }
-}  // extern "C"
+  xnn_run_operator(add_op, nullptr /* thread pool */);
+}
 }  // namespace wasm
 }  // namespace mindspore
 
 int main() {
     std::cout<<"hello, world"<<std::endl;
+    mindspore::wasm::Add();
     return 0;
 }
