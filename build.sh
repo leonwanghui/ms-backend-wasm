f4c406adb0ce60dc068b43c28c0fbded8458313d
leonwanghui <wanghui71leon@gmail.com>
2020-05-13 16:27:22 +0800

    Remove xnnpack 3rd package and add wasmtime package

--- build.sh
+++ build.sh
@@ -11,18 +11,10 @@ export CC="${WASI_SDK_PATH}/bin/clang --sysroot=${WASI_SDK_PATH}/share/wasi-sysr
 export CXX="${WASI_SDK_PATH}/bin/clang++ --sysroot=${WASI_SDK_PATH}/share/wasi-sysroot"
 
 CMAKE_SOURCE_DIR=$(cd "$(dirname $0)"; pwd)
-XNNPACK_SOURCE_DIR=${CMAKE_SOURCE_DIR}/third_party/xnnpack
-
-if [ ! -d "${XNNPACK_SOURCE_DIR}/build" ]; then
-    mkdir -p ${XNNPACK_SOURCE_DIR}/build
-fi
-cd ${XNNPACK_SOURCE_DIR}/build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=./install ..
-cmake --build . --target install -j4
-
 if [ ! -d "${CMAKE_SOURCE_DIR}/build" ]; then
     mkdir -p ${CMAKE_SOURCE_DIR}/build
 fi
+
 cd ${CMAKE_SOURCE_DIR}/build
 cmake -D CMAKE_CXX_FLAGS="-fno-exceptions -fno-rtti" ..
 cmake --build . -j4
